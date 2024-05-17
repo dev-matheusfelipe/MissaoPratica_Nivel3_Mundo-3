ccfec931caa57b12a0b0ef5ca89b40e592ea2936
Matheus Felipe <64490699+dev-matheusfelipe@users.noreply.github.com>
2024-05-17 20:16:56 -0300

    Add files via upload

---
+++ CadastroBD/src/PreparedStatement.java
@@ -0,0 +1,16 @@
+
+class PreparedStatement {
+
+    void setString(int i, String nome) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    void setInt(int i, int telefone) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
